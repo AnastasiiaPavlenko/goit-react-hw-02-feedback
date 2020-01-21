4bd5246d92427a9ddb725c82d234ce4fc0477491
Anastasiia Pavlenko <anastasia2ustimets@gmail.com>
2020-01-21 16:45:10 +0200

    Updates

---
+++ precache-manifest.09782ee2e991a2a2ba2cda2dac2bdf2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b0e742dbd8618b1c21cf0c2d56e3cc",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "a9f890d8cb3e8272b1cf",
+    "url": "/goit-react-hw-02-feedback/static/css/main.2e46679d.chunk.css"
+  },
+  {
+    "revision": "6454ffa78860d965afb6",
+    "url": "/goit-react-hw-02-feedback/static/js/2.660ccc28.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.660ccc28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9f890d8cb3e8272b1cf",
+    "url": "/goit-react-hw-02-feedback/static/js/main.78366457.chunk.js"
+  },
+  {
+    "revision": "95c2552a26ef56e6553b",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.1a889cda.js"
+  }
+]);
